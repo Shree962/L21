45010ca43e7f4c69ed415848b8a1e13ed48dc1aa
Shree962 <shreetha.h@s.amity.edu>
2025-02-19 19:23:57 +0530

    modifying the program

--- new1.c
+++ new1.c
@@ -2,9 +2,9 @@
 void main()
 {
   int a=12,b=32;
-  printf("%d",a);
-  printf("%d",b);
-  printf("%d",a+b);
+  printf("%d\n",a);
+  printf("%d\n",b);
+  printf("%d\n",a+b);
 
-  printf("%d",a*b);	  
+  printf("%d\n",a*b);	  
 }
