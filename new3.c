479da34bb3f0f1071e1895c6ce30848b83f3f3a2
Shree962 <shreetha.h@s.amity.edu>
2025-03-08 14:11:45 +0530

    Print area of the circle

--- new3.c
+++ new3.c
@@ -4,3 +4,10 @@ int main() {
 	float radius,area;
         printf("Enter the radius of circle:");
 	scanf("%f",&radius);
+
+	area=PI*radius*radius;
+
+	printf("The area of the circle is: %2f\n",area);
+
+	return 0;
+}
