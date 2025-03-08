98e43f51e79667a95d88f34771cccf0d2d911fc4
Shree962 <shreetha.h@s.amity.edu>
2025-03-08 13:34:49 +0530

    Added case 1 to case 3 in switch statement

--- new2.c
+++ new2.c
@@ -4,4 +4,13 @@ int day;
 printf("Enter a number (1-7):");
 scanf("%d",&day);
 
-
+witch (day) {
+	case 1:
+		printf("Sunday\n");
+		break;
+	case 2: 
+		printf("Monday\n");
+		break;
+	case 3:
+		printf("Tuesday\n");
+		break;
